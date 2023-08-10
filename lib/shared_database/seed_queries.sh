7643a8271604817c9b8ca58b9aceca7693c3baa3
HavierHeng <javier_2001@live.com.sg>
2023-08-11 05:03:25 +0800

    Added an alternative note to add queries from queries.txt

--- lib/shared_database/seed_queries.sh
+++ lib/shared_database/seed_queries.sh
@@ -7,3 +7,10 @@ do
    rake query:add "$query"
 done
 
+
+# From bundle exec irb, faster than using the above bash script
+# require "shared_database"
+# queries = IO.readlines("#{Dir.pwd}/lib/shared_database/queries.txt")
+# queries = queries.map(&:strip)
+# queries = queries.map{ |query| {"query": query} }
+# Query.create(queries)
